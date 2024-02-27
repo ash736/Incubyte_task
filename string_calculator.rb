54f166a2c5c9c18475072d767c82118c4d1170ee
ashish <ashishbhagore.ab@gmail.com>
2024-02-27 12:21:27 +0530

    Case for single and sum of numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,5 +3,8 @@ require 'byebug'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
+    return numbers.to_i if numbers.size == 1
+    nums = numbers.split(",").map(&:to_i)
+    nums.reduce(:+)
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -8,5 +8,18 @@ describe StringCalculator do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+    context "when the input is a single number" do
+      it "returns the number itself" do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+    end
+
+     context "when the input contains two numbers separated by a comma" do
+      it "returns the sum of the numbers" do
+        expect(StringCalculator.add("1,5")).to eq(6)
+      end
+    end
+
   end
 end
