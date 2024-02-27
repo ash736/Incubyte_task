fcc5ded5ea9f711385aad7e42b40954644ddd4a3
ashish <ashishbhagore.ab@gmail.com>
2024-02-27 12:06:25 +0530

    Add ruby file and check for empty case

---
+++ string_calculator.rb
@@ -0,0 +1,7 @@
+require 'byebug'
+
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
+  end
+end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,1 +1,12 @@
 require 'rspec'
+require_relative 'string_calculator'
+
+describe StringCalculator do
+  describe ".add" do
+    context "when the input is an empty string" do
+      it "returns 0" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
+end
